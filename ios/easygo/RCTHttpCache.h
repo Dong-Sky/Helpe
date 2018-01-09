580cfe99aecd1ad7dfc140dea7f0e9a65f71c540
ycyc1996 <answer_yyc@163.com>
2018-01-09 22:05:18 +0800

    ios 1.0.0

---
+++ ios/easygo/RCTHttpCache.h
@@ -0,0 +1,18 @@
+//
+//  RCTHttpCache.h
+//  cache
+//
+//  Created by answer on 2017/11/18.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+#ifndef RCTHttpCache_h
+#define RCTHttpCache_h
+
+
+#endif /* RCTHttpCache_h */
+
+#import <React/RCTBridgeModule.h>
+@interface RCTHttpCache : NSObject<RCTBridgeModule>
+
+@end
